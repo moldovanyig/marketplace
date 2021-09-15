b9d5389000ca8f7d624bfa69aa26cf455835054e
moldovanyig <moldovanyi.gergely@gmail.com>
2021-09-15 22:23:24 +0200

    remove App.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 
 import Login from './components/login';
-import './App.css';
 
 library.add(faExclamationTriangle);
 
