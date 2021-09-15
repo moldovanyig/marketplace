0c9790627d3c97981f6aa5f69541c3e27285f5d5
moldovanyig <moldovanyi.gergely@gmail.com>
2021-09-15 21:49:07 +0200

    add service for login

---
+++ frontend/src/services/authService.ts
@@ -0,0 +1,31 @@
+import { LoginInfo } from '../interfaces/login';
+import { userInfo } from '../interfaces/user';
+
+import config from '../config';
+
+const authService = async (loginData: userInfo): Promise<LoginInfo> => {
+  if (!loginData.name || !loginData.password) {
+    return {
+      error: 'All the input fields are required.',
+    };
+  }
+  try {
+    const response = await fetch(`${config.url}/api/login`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(loginData),
+    });
+    const result = await response.json();
+
+    if (result.status === 'error') return { error: result.message };
+    else {
+      return {
+        authorization: result.authorization,
+      };
+    }
+  } catch (err: any) {
+    return err.message;
+  }
+};
+
+export { authService };
