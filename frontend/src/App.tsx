af301b4a74fef819b0b599aa4dc16b9ed9bf1519
moldovanyig <moldovanyi.gergely@gmail.com>
2021-09-17 14:28:48 +0200

    add Provider & store

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,29 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 
 import Login from './components/login';
 import Registration from './components/registration';
+import store from './store';
 
 library.add(faExclamationTriangle);
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/registration">
-          <Registration />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/registration">
+            <Registration />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
