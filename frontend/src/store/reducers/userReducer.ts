940f168c969c43ffe338fea6e6de714d1404c078
moldovanyig <moldovanyi.gergely@gmail.com>
2021-09-18 11:27:21 +0200

    add avatar&money

--- frontend/src/store/reducers/userReducer.ts
+++ frontend/src/store/reducers/userReducer.ts
@@ -3,11 +3,15 @@ import { SAVE_USER_INFO } from '../actions/userAction';
 interface UserState {
   authorization: string;
   name: string;
+  avatar: number;
+  money: number;
 }
 
 const initialState: UserState = {
   authorization: '',
   name: '',
+  avatar: 0,
+  money: 0,
 };
 
 export const userReducer = (state = initialState, action: any) => {
