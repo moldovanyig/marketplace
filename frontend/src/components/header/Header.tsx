a5272cf46960bccb6b1c08859fc02ae9bc3db449
moldovanyig <moldovanyi.gergely@gmail.com>
2021-09-18 11:25:22 +0200

    add avatars

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,16 +1,54 @@
-import { FC, useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import store from '../../store';
 
 import './Header.css';
 
+import Avatar1 from '../../assets/avatars/1.png';
+import Avatar2 from '../../assets/avatars/2.png';
+import Avatar3 from '../../assets/avatars/3.png';
+import Avatar4 from '../../assets/avatars/4.png';
+import Avatar5 from '../../assets/avatars/5.png';
+import Avatar6 from '../../assets/avatars/6.png';
+import Avatar7 from '../../assets/avatars/7.png';
+import Avatar8 from '../../assets/avatars/8.png';
+import Avatar9 from '../../assets/avatars/9.png';
+import Avatar10 from '../../assets/avatars/10.png';
+import Avatar11 from '../../assets/avatars/11.png';
+import Avatar12 from '../../assets/avatars/12.png';
+
 interface HeaderProps {
   authorization: string;
   name: string;
+  avatar: number;
+  money: number;
 }
 
-const Header: FC<HeaderProps> = ({ authorization, name }) => {
+const Header: React.FunctionComponent<HeaderProps> = ({
+  authorization,
+  name,
+  avatar,
+  money,
+}) => {
+  console.log(avatar);
+
   const [headerLoggedIn, setHeaderLoggedIn] = useState(false);
+  const avatars = [
+    Avatar1,
+    Avatar2,
+    Avatar3,
+    Avatar4,
+    Avatar5,
+    Avatar6,
+    Avatar7,
+    Avatar8,
+    Avatar9,
+    Avatar10,
+    Avatar11,
+    Avatar12,
+  ];
+
+  let choosenAvatar = useRef(avatars[avatar]);
 
   useEffect(() => {
     const checkStorage = (): void => {
@@ -39,9 +77,11 @@ const Header: FC<HeaderProps> = ({ authorization, name }) => {
         </>
       ) : (
         <>
-          <Link to="/">
-            <h1>{name}</h1>
-          </Link>
+          <h1>
+            <img src={choosenAvatar.current} alt="avatar" />
+            Hello {name}, you have {money} credit.
+          </h1>
+
           <div className="header-links">
             <Link onClick={handleClick} to="/login">
               Logout
