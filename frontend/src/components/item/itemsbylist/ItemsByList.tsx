c72a86eef3bb49bbf03885269c0d37a0b8e04979
moldovanyig <moldovanyi.gergely@gmail.com>
2021-09-27 20:52:41 +0200

    add two more divs

--- frontend/src/components/item/itemsbylist/ItemsByList.tsx
+++ frontend/src/components/item/itemsbylist/ItemsByList.tsx
@@ -6,17 +6,24 @@ interface ItemsByListProps {
 
 const ItemsByList: React.FunctionComponent<ItemsByListProps> = (items) => {
   return (
-    <div>
-      {items &&
-        items.items.map((item, index) => {
-          return (
-            <div className="card" key={`item-${index}`}>
-              <h1 className="card-title">{item.title}</h1>
-              <img src={item.photo_url} alt={item.photo_url} />
-              <p className="card-body">Cost: {item.price} credit.</p>
-            </div>
-          );
-        })}
+    <div className="container">
+      <div className="row gap-2">
+        {items &&
+          items.items.map((item, index) => {
+            return (
+              <div
+                className="col-12-xs col-5-sm col-3-xl"
+                key={`item-${index}`}
+              >
+                <div className="card">
+                  <h1 className="card-title">{item.title}</h1>
+                  <img src={item.photo_url} alt={item.photo_url} />
+                  <p className="card-body">Cost: {item.price} credit.</p>
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
